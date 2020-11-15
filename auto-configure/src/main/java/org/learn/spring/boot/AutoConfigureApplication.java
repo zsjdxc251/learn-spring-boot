1ff1cc857caf4b5d522fde6c65d1ae8250facc24
zhengsj <zhengsj@akulaku.com>
2020-11-16 00:10:25 +0800

    添加指引类

---
+++ auto-configure/src/main/java/org/learn/spring/boot/AutoConfigureApplication.java
@@ -0,0 +1,15 @@
+package org.learn.spring.boot;
+
+import org.springframework.core.io.support.SpringFactoriesLoader;
+
+/**
+ *
+ *
+ *  {@link SpringFactoriesLoader} 加载  META-INF/spring.factories 文件
+ *
+ *
+ * @author zhengshijun
+ * @version created on 2020/11/16.
+ */
+public class AutoConfigureApplication {
+}
--- auto-configure/src/test/java/org/learn/spring/boot/AppTest.java
+++ auto-configure/src/test/java/org/learn/spring/boot/AppTest.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.learn.spring.boot;
 
 import static org.junit.Assert.assertTrue;
 
